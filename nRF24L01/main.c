b5a9c904df32b17c5a2b03b125b3b0400947508b
Hooterr <maxus1999@gmail.com>
2018-02-08 20:07:57 +0100

    Initial

---
+++ nRF24L01/main.c
@@ -0,0 +1,18 @@
+/*
+ * nRF24L01.c
+ *
+ * Created: 08/02/2018 20:06:12
+ * Author : maxus
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
